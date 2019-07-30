d608125351e93467f852007f34a8bda36d474b19
Ravindar Cheganti <ravindar_cheganti@epam.com>
2019-07-30 22:32:43 +0530

    updated store and movie container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import { ErrorBoundary } from './ErrorsBoundary/ErrorBoundary';
 import Routes from './Routes';
 
 const App = () => (
-  <ErrorBoundary>
-    <Routes />
-  </ErrorBoundary>
+  <Provider store={store}>
+    <ErrorBoundary>
+      <Routes />
+    </ErrorBoundary>
+  </Provider>
 );
 export default App;
---
+++ src/components/MovieContainer/MovieContainerReducer.js
@@ -0,0 +1,38 @@
+import { UPDATE_INPUT_VALUE,UPDATE_SEARCH_RESULTS, MOVIES_API_FETCH } from './MovieContainerActions';
+
+const initialState = {
+  searchResults: [],
+  updatedList: [],
+  inputValues: '',
+  isTitle: false,
+  isGenres: false,
+  isRelease: true,
+  isRating: false,
+};
+
+const movieContainerReducer = (state = initialState, action) => {
+  console.log(state);
+  switch (action.type) {
+    case UPDATE_INPUT_VALUE:
+      return {
+        ...state,
+        inputValues: action.payload,
+      };
+    case MOVIES_API_FETCH:
+      return {
+        ...state,
+        searchResults: action.payload,
+      };
+
+    case UPDATE_SEARCH_RESULTS:
+      console.log(action.payload);
+      return {
+        ...state,
+        searchResults: action.payload,
+      }
+    default:
+      return state;
+  }
+}
+
+export default movieContainerReducer;
